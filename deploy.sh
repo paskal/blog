cfa2c1af71a0aa4ad13089539e4bab5505ea32ef
Dmitry Verkhoturov <paskal.07@gmail.com>
2025-05-10 14:15:47 +0100

    Add optional --path parameter to deploy.sh for subdirectory deployment
    Former-commit-id: 5956de6d872bd9ae9804a174a3da26db15b66168

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,30 @@
 #!/bin/sh
 
-hugo --minify --cleanDestinationDir && rsync -azi --exclude cv/ --delete public/ terrty:~/blog/public/ | grep -v "f..t...." | grep -v ".d..t...."
+# Parse arguments
+SUBDIR=""
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --path)
+      SUBDIR="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
+# Build site
+hugo --minify --cleanDestinationDir
+
+# Set target path
+TARGET="terrty:~/blog/public/"
+if [ -n "$SUBDIR" ]; then
+  TARGET="terrty:~/blog/public/$SUBDIR/"
+  # Create directory if it doesn't exist
+  ssh terrty "mkdir -p ~/blog/public/$SUBDIR"
+fi
+
+# Deploy
+rsync -azi --exclude cv/ --delete public/ "$TARGET" | grep -v "f..t...." | grep -v ".d..t...."
